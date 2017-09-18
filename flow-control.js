b54a6d5158eb9b08f2040957f187d67bc6fb152f
Chris Nady <nady.christopher@gmail.com>
2017-09-18 19:13:40 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-  return age>=13 && age <=19? "You are a teenager" : "You are not a teenager"
+  return age>=13 && age <=19? "You are a teenager!" : undefined
 }
 
 function teenager(age) {
